f416114e2804149b50268032d58a06969b4a0cc9
Vysakh Prasanth <vysakhnair0@hotmail.com>
2023-07-21 01:49:07 -0400

    Initial Commit

---
+++ fortywinks/fortywinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  fortywinksApp.swift
+//  fortywinks
+//
+//  Created by Vysakh Prasanth on 2023-07-21.
+//
+
+import SwiftUI
+
+@main
+struct fortywinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
